aeb78d7ed261dd1b00d5359db5eba49b941ed357
Uri Rahimi <urahimi@usc.edu>
2018-12-02 18:30:18 -0800

    added form validation

--- app/controllers/create.js
+++ app/controllers/create.js
@@ -5,17 +5,28 @@ export default Controller.extend({
     createTodo(e) {
       e.preventDefault();
 
-      let todo = this.store.createRecord("todo", {
-        dateMade: this.dateMade,
-        dueDate: this.dueDate,
-        subject: this.subject,
-        message: this.message,
-        completed: this.completed
-      });
+      // Form Validation
+      if (this.dateMade === "") {
+        $("#dateMadeError").text("Input cannot be empty");
+      } else if (this.dueDate === "") {
+        $("#dueDateError").text("Input cannot be empty");
+      } else if (this.subject === "") {
+        $("#subjectError").text("Input cannot be empty");
+      } else if (this.message === "") {
+        $("#messageError").text("Input cannot be empty");
+      } else {
+        let todo = this.store.createRecord("todo", {
+          dateMade: this.dateMade,
+          dueDate: this.dueDate,
+          subject: this.subject,
+          message: this.message,
+          completed: this.completed
+        });
 
-      todo.save().then(() => {
-        this.transitionToRoute("index");
-      });
+        todo.save().then(() => {
+          this.transitionToRoute("index");
+        });
+      }
     }
   }
 });
