56c84e50d3434fcb1d7523eef67f8aab27710da5
Uri Rahimi <urahimi@usc.edu>
2018-12-02 17:15:28 -0800

    fixed deleting bug

--- app/controllers/create.js
+++ app/controllers/create.js
@@ -2,7 +2,7 @@ import Controller from "@ember/controller";
 
 export default Controller.extend({
   actions: {
-    createEmail(e) {
+    createTodo(e) {
       e.preventDefault();
 
       let todo = this.store.createRecord("todo", {
